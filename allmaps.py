0a08688204ac9991f384c49aada9c719fdecb0da
d0gb0ne <d0gb0ne@users.noreply.github.com>
2016-10-10 00:20:22 +0200

    Create allmaps.py

---
+++ allmaps.py
@@ -0,0 +1,127 @@
+#Allmaps
+import minqlx
+
+class allmaps(minqlx.Plugin):
+    def __init__(self):
+        self.add_command(("allmaps"), self.cmd_allmaps)
+
+    def cmd_allmaps(self, player, msg, channel):
+        player.tell("oxodm102_b1: Campgrounds Redux\n"
+                "karist: mid teamsize CA\n"
+                "6ex: Campgrounds After\n"
+                "prodcmap7: Fortress 2089 (Hidden Fortress remake)\n"
+                "ts_dm5 / ts_dm5tmp (bigger): Full Moon - Ufreeze Map\n"
+                "nodm10: The Edge 2004 CPM Remix\n"
+                "szq2dm1ish: Q2DM1-ISH: The Edge Remix\n"
+                "p1rate_overek: Pirate Overkill Cut em' Hard\n"
+                "6plus: 6+ (Q3: 6++)\n"
+                "q1dm6t: The Dark Zone 2020\n"
+                "oxodm2a: Aero Run (Second Edition)\n"
+                "shovq2dm1_cpm: The Edge, Revisited!\n"
+                "nalq1dm6: The Sinister Zone (q1dm6)\n"
+                "proverek: Pro Overkill / Projectile Vomiting\n"
+                "dm6remix: Q3DM6 In a hat\n"
+                "ql_dust, ql_dust2: CS maps\n"
+                "Opium: Opium\n"
+                "heroscerated: Heroscerated\n"
+                "ts_ca1: Fly on Rocket\n"
+                "heffdm17: DM17 Egyptian Redux\n"
+                "dAdeX: dAde_X\n"
+                "e1m7ish3: remake of Quake's E1M7\n"
+                "ci: Citadel Insatiable\n"
+                "Arena66: Arena66\n"
+                "2sweet: 2!Sweet large map good for 6vs6\n"
+                "dubneoc: Neorganic Epiphany Sock and Nunuks Geometric Challenge 2\n"
+                "hyperblast: Hyperblast\n"
+                "ct3dm4: Paranoid  3vs3\n"
+                "lsjgcoop: MaXimus 4-8 players\n"
+                "q3dm4remix: Eviscerated remix\n"
+                "q3dm13remix: Lost World remix\n"
+                "q3dm1+: The Arena Gate II\n"
+                "damn_q3dm1: Arena Gate (damn edition)\n"
+                "6ex: Campgrounds After (q3dm6 upgraded)\n"
+                "prodcmap7: Fortress 2089 (Hidden Fortress remake)\n"
+                "heroscerated: Heroscerated\n"
+                "inversekill: Inversekill\n"
+                "13matrix: Campgrounds - Matrix Edition\n"
+                "gd_tourney1a: Thrill Kill\n"
+                "Ra3map11pp: Overkill PP\n"
+                "Ra3map2pp: ShakenNotStirredPP The ProPack Version\n"
+                "ra3map1: Theatre of Pain by Firestarter\n"
+                "ra3map1a: Evolution by Firestarter\n"
+                "ra3map1b: Thunderstruck by Firestarter\n"
+                "ra3map1c: Canned Heat by Firestarter\n"
+                "ospca1: American Can-CO\n"
+                "ospctf1: White Noise\n"
+                "ospctf2: Crossed Paths\n"
+                "ospdm1: The New Edge\n"
+                "ospdm3: Scrap Metal II\n"
+                "ospdm4: Bitter Embrace\n"
+                "ospdm2: Batcula\n"
+                "ospdm5: Deep Inside\n"
+                "ospdm6: Suicide\n"
+                "ospdm7: The Dancehall\n"
+                "ospdm8: The Chastity Belt\n"
+                "ospdm9: Anticipating Oblivion\n"
+                "ospdm10: Apologie\n"
+                "ospdm12: Deschutes\n"
+                "ospdm11: The Olden Domain\n"
+                "ospdm13: Bullet Ride\n"
+                "ospdm14: Epilogue\n"
+                "ospdm15: Deeper Blue\n"
+                "\n"
+                "")
+        return minqlx.RET_STOP_ALL
+        
+'''
+560545593
+623436886
+632421088
+632254333
+575312620
+563105825
+560535691
+560533896
+560531568
+560523904
+557591894
+550566693
+547252823
+540986862
+539421982
+539421606
+607909342
+560547573
+560548198
+560550879
+560610357
+560647562
+572015381
+586817666
+605561819
+606599547
+597920398
+638619897
+638619355
+638618725
+638618287
+638617899
+638616850
+638531198
+638500202
+637351306
+637350852
+632422741
+632422465
+632422201
+632421804
+632421451
+632421088
+632254333
+623436886
+606599547
+600299201
+691078677
+668999226
+728480172
+'''
